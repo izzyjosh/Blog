80913497f50b5d6a8c768ac84f856724c7f443cb
izzyjosh <calebayuba222@gmail.com>
2024-04-26 17:51:55 +0100

    Added new changes

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,8 @@ app_name = "main"
 
 urlpatterns = [
         path("",views.index,name="index"),
+        path("category/<int:cat_id>/",views.category,name="category"),
+        path("post/<int:post_id>/",views.post_detail, name="post"),
         path("profile/",views.profile,name="profile"),
-        path("post/",views.post,name="post"),
         ]
 
--- main/views.py
+++ main/views.py
@@ -1,33 +1,46 @@
-#django import 
-from django.shortcuts import render,redirect
-from django.http import HttpRequest
+# django import
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest,HttpResponse
 
-#my import 
-from .models import Article,Category
+# my import
+from .models import Article, Category
 from .forms import PostForm
 
-#index page view
-def index(request:HttpRequest):
-    categories:QuerySet = Category.objects.all()
-    posts:QuerySet = Article.objects.all()
-    editors_pic = Article.objects.all()[:3]
-    trending = Article.objects.order_by("?")[:3]
+
+# index page view
+def index(request: HttpRequest) -> HttpResponse:
+    categories: QuerySet = Category.objects.all()
+    posts: QuerySet = Article.objects.all()
 
     context = {
-            "categories":categories,
-            "posts":posts,
-            "editors_pic":editors_pic,
-            "trending":trending
+        "categories": categories,
+        "posts": posts,
+    }
+    return render(request, "index.html", context)
+
+
+#when a category is specified
+def category(request:HttpRequest, cat_id:int) -> HttpResponse:
+    cat_post: Article = Article.objects.filter(category_id=cat_id)
+
+    context = {
+            "cat_posts":cat_post,
             }
     return render(request,"index.html",context)
 
 
+#viewing each post
+def post_detail(request:HttpRequest,post_id:int) -> HttpResponse:
+    post: Article = get_object_or_404(Article,pk=post_id)
 
-def profile(request:HttpRequest):
+    return render(request,"post.html",{"post":post})
+
+
+def profile(request: HttpRequest):
     if request.method == "POST":
-        first_name:str = request.POST.get("first_name")
-        last_name:str = request.POST.get("last_name")
-        contact:str = request.POST.get("contact")
+        first_name: str = request.POST.get("first_name")
+        last_name: str = request.POST.get("last_name")
+        contact: str = request.POST.get("contact")
         img = request.POST.get("img")
 
         user = request.user
@@ -39,36 +52,33 @@ def profile(request:HttpRequest):
         user.save()
         return redirect("main:post")
 
-
-    return render(request,"profile.html")
-
+    return render(request, "profile.html")
 
 
-def post(request:HttpRequest):
-    if request.user.first_name == None and request.user.last_name== None:
+def post(request: HttpRequest):
+    if request.user.first_name == None and request.user.last_name == None:
         return redirect("main:profile")
 
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            title:str = request.POST.get("title")
-            body:str = request.POST.get("body")
+            title: str = request.POST.get("title")
+            body: str = request.POST.get("body")
             category = request.POST.get("category")
             img = request.POST.get("img")
 
             cat = Category.objects.get(id=category)
 
             Article.objects.create(
-                    title=title,
-                    body=body,
-                    category=cat,
-                    images=img,
-                    author=request.user,)
+                title=title,
+                body=body,
+                category=cat,
+                images=img,
+                author=request.user,
+            )
 
             return redirect("main:index")
 
     else:
         form = PostForm()
-    return render(request,"post.html",{"form":form})
-
-
+    return render(request, "post.html", {"form": form})
