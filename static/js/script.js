2b1e5630681955a3e17b995f7ec2392f43aaadd3
Joseph Joshua <calebayuba222@gmail.com>
2026-01-30 12:40:50 +0100

    clean

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function showSideBar() {
- const sideBar = document.querySelector(".sidebar");
- sideBar.style.display = "flex"
-}
-
-function closeSideBar() {
- const sideBar = document.querySelector(".sidebar")
- sideBar.style.display = "none"
-}
